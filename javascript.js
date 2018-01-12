33d479f19a04aa8e795c1c405d6903507bbc3f72
aaron <35290478+keechingchew@users.noreply.github.com>
2018-01-12 17:59:02 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,29 @@
+ //function turnToRed{
+ //document.getElementById('id1').style.color = 'red'; }//
+
+console.log("hellloo");
+
+
+// const hello = document.getElementById('id1');
+// console.log(hello)
+// hello.style.color = 'red';
+
+
+
+document.getElementById('btn').onclick = function(){ turnToRed() }; 
+
+function turnToRed(){
+	const hello = document.getElementById('id1');
+	hello.style.color = 'red';
+}
+
+// document.write('My JavaScript is alive')
+
+// alert('Hello its A')
+// alert('Dont give up')
+// turnToRed();
+
+
+
+
+
